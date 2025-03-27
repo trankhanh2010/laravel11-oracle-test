96aca3bafcf43394ffd3fca331d2fbca1a51c623
trankhanh2010 <tranlenguyenkhanh20102001@gmail.com>
2025-03-27 13:14:38 +0700

    upcode

--- config/database.php
+++ config/database.php
@@ -276,74 +276,54 @@ return [
         'expire' => 86400, // Thời gian hết hạn mặc định cho cache (đơn vị: giây)
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        // 'options' => [
-        //     'name' => env('APP_NAME', 'laravel'),  // Đặt tên cho kết nối
-        //     'cluster' => env('REDIS_CLUSTER', 'redis'),
-        //     // 'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        //     'prefix' => "",
-        //     'read_timeout' => 2.5
-        // ],
-
-        // 'default' => [
-        //     'url' => env('REDIS_URL'),
-        //     'host' => env('REDIS_HOST', '127.0.0.1'),
-        //     'password' => env('REDIS_PASSWORD', null),
-        //     'username' => env('REDIS_USERNAME', null),
-        //     'port' => env('REDIS_PORT', '6379'),
-        //     'database' => env('REDIS_DB', '0'),
-        // ],
-
-        // 'cache' => [
-        //     'url' => env('REDIS_URL'),
-        //     'host' => env('REDIS_HOST', '127.0.0.1'),
-        //     'password' => env('REDIS_PASSWORD', null),
-        //     'username' => env('REDIS_USERNAME', null),
-        //     'port' => env('REDIS_PORT', '6379'),
-        //     'database' => env('REDIS_CACHE_DB', '1'),
-        // ],
         'options' => [
-            'name' => env('APP_NAME', 'laravel'),
-            'cluster' => false,
-            'replication' => 'sentinel',
-            'service' => env('REDIS_SENTINEL_SERVICE', 'my-master-sentine'),
+            'name' => env('APP_NAME', 'laravel'),  // Đặt tên cho kết nối
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            // 'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
             'prefix' => "",
+            'read_timeout' => 2.5
         ],
 
         'default' => [
-            'sentinel_host' => env('REDIS_SENTINEL_HOST', '127.0.0.1'),
-            'sentinel_port' => (int) env('REDIS_SENTINEL_PORT', 26379),
-            'sentinel_service' => env('REDIS_SENTINEL_SERVICE', 'my-master-sentinel'),
-            'sentinel_timeout' => (float) env('REDIS_SENTINEL_TIMEOUT', 0),
-            'sentinel_persistent' => env('REDIS_SENTINEL_PERSISTENT'),
-            'sentinel_retry_interval' => (int) env('REDIS_SENTINEL_RETRY_INTERVAL', 0),
-            'sentinel_read_timeout' => (float) env('REDIS_SENTINEL_READ_TIMEOUT', 0),
-            'sentinel_username' => env('REDIS_USERNAME'),
-            'sentinel_password' => env('REDIS_PASSWORD'),
-
-            // 'url' => env('REDIS_URL'),
-            // 'host' => env('REDIS_SENTINEL_HOST', 'my-redis-sentinel'),
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_SENTINEL_PORT', 26379),
-            'database' => 0,
+            'username' => env('REDIS_USERNAME', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
-            'sentinel_host' => env('REDIS_SENTINEL_HOST', '127.0.0.1'),
-            'sentinel_port' => (int) env('REDIS_SENTINEL_PORT', 26379),
-            'sentinel_service' => env('REDIS_SENTINEL_SERVICE', 'my-master-sentinel'),
-            'sentinel_timeout' => (float) env('REDIS_SENTINEL_TIMEOUT', 0),
-            'sentinel_persistent' => env('REDIS_SENTINEL_PERSISTENT'),
-            'sentinel_retry_interval' => (int) env('REDIS_SENTINEL_RETRY_INTERVAL', 0),
-            'sentinel_read_timeout' => (float) env('REDIS_SENTINEL_READ_TIMEOUT', 0),
-            'sentinel_username' => env('REDIS_USERNAME'),
-            'sentinel_password' => env('REDIS_PASSWORD'),
-
-            // 'url' => env('REDIS_URL'),
-            'host' => env('REDIS_SENTINEL_HOST', 'my-redis-sentinel'),
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_SENTINEL_PORT', 26379),
-            'database' => 1,
+            'username' => env('REDIS_USERNAME', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
         ],
+        // 'options' => [
+        //     'name' => env('APP_NAME', 'laravel'),
+        //     'cluster' => false,
+        //     'replication' => 'sentinel',
+        //     'service' => env('REDIS_SENTINEL_SERVICE', 'my-master-sentine'),
+        //     'prefix' => "",
+        // ],
+
+        // 'default' => [
+        //     // 'url' => env('REDIS_URL'),
+        //     'host' => env('REDIS_SENTINEL_HOST', 'my-redis-sentinel'),
+        //     'password' => env('REDIS_PASSWORD', null),
+        //     'port' => env('REDIS_SENTINEL_PORT', 26379),
+        //     'database' => 0,
+        // ],
+
+        // 'cache' => [
+        //     // 'url' => env('REDIS_URL'),
+        //     'host' => env('REDIS_SENTINEL_HOST', 'my-redis-sentinel'),
+        //     'password' => env('REDIS_PASSWORD', null),
+        //     'port' => env('REDIS_SENTINEL_PORT', 26379),
+        //     'database' => 1,
+        // ],
 
     ],
 
