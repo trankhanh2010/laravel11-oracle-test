418b737a658288fd1e6868a5a11cac125edeadbd
trankhanh2010 <tranlenguyenkhanh20102001@gmail.com>
2025-05-15 09:52:59 +0700

    +fix check hoan ung, tt kc khi huy transaction

--- app/Http/Requests/Transaction/UpdateCancelTransactionRequest.php
+++ app/Http/Requests/Transaction/UpdateCancelTransactionRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Transaction;
 
 use App\Models\HIS\PayForm;
 use App\Models\HIS\Transaction;
+use App\Models\HIS\TransactionType;
 use App\Models\HIS\Treatment;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -19,6 +20,10 @@ class UpdateCancelTransactionRequest extends FormRequest
     protected $treatment;
     protected $payFormQrId;
     protected $payForm;
+    protected $transactionType;
+    protected $transactionTypeHUId;
+    protected $transactionTypeTUId;
+    protected $transactionTypeTTId;
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -37,11 +42,36 @@ class UpdateCancelTransactionRequest extends FormRequest
         $this->transaction = new Transaction();
         $this->treatment = new Treatment();
         $this->payForm = new PayForm();
+        $this->transactionType = new TransactionType();
 
         $cacheKeySet = "cache_keys:" . "setting"; // Set để lưu danh sách key
         $cacheKey = 'pay_form_qr_vietin_bank_id';
         $this->payFormQrId = Cache::remember($cacheKey, now()->addMinutes(10080), function () {
             $data =  $this->payForm->where('pay_form_code', '08')->get();
+            return $data->value('id');
+        });
+        // Lưu key vào Redis Set để dễ xóa sau này
+        Redis::connection('cache')->sadd($cacheKeySet, [$cacheKey]);
+
+        $cacheKey = 'transaction_type_HU_id';
+        $this->transactionTypeHUId = Cache::remember($cacheKey, now()->addMinutes(10080), function () {
+            $data =  $this->transactionType->where('transaction_type_code', 'HU')->get();
+            return $data->value('id');
+        });
+        // Lưu key vào Redis Set để dễ xóa sau này
+        Redis::connection('cache')->sadd($cacheKeySet, [$cacheKey]);
+
+        $cacheKey = 'transaction_type_TU_id';
+        $this->transactionTypeTUId = Cache::remember($cacheKey, now()->addMinutes(10080), function () {
+            $data =  $this->transactionType->where('transaction_type_code', 'TU')->get();
+            return $data->value('id');
+        });
+        // Lưu key vào Redis Set để dễ xóa sau này
+        Redis::connection('cache')->sadd($cacheKeySet, [$cacheKey]);
+        
+        $cacheKey = 'transaction_type_TT_id';
+        $this->transactionTypeTTId = Cache::remember($cacheKey, now()->addMinutes(10080), function () {
+            $data =  $this->transactionType->where('transaction_type_code', 'TT')->get();
             return $data->value('id');
         });
         // Lưu key vào Redis Set để dễ xóa sau này
@@ -106,6 +136,34 @@ class UpdateCancelTransactionRequest extends FormRequest
                 if ($this->cancel_time && $this->cancel_time < $dataTransaction->transaction_time) {
                     $validator->errors()->add('cancel_time', 'Thời gian hủy không được nhỏ hơn thời gian giao dịch!');
                 }
+                // nếu là tạm thu => Check xem sau giao dịch này (id > hơn id transaction này) có giao dịch hoàn ứng hay giao dịch thanh toán nào mà có kc không, nếu có => không cho hủy
+                if ($dataTransaction->transaction_type_id == $this->transactionTypeTUId) {
+                    $exists = $this->transaction
+                        ->where('his_transaction.treatment_id', $dataTransaction->treatment_id)
+                        ->where('his_transaction.id', '>', $dataTransaction->id)
+                        ->where(function ($q) {
+                            $q->where(function ($q1) {
+                                // Giao dịch hoàn ứng
+                                $q1->where('his_transaction.transaction_type_id', $this->transactionTypeHUId);
+                            })
+                            ->orWhere(function ($q2) {
+                                // Giao dịch thanh toán và có kc_amount > 0
+                                $q2->where('his_transaction.transaction_type_id', $this->transactionTypeTTId)
+                                    ->where('his_transaction.kc_amount', '>', 0);
+                            });
+                        })
+                        ->where(function ($q) {
+                            // Kiểm tra is_cancel null hoặc 0
+                            $q->whereNull('his_transaction.is_cancel')
+                              ->orWhere('his_transaction.is_cancel', 0);
+                        })
+                        ->exists();
+                
+                    if ($exists) {
+                        $validator->errors()->add('sere_servs', 'Xử lý thất bại! Số tiền đã được hoàn ứng hoặc kết chuyển một phần!');
+                    }
+                }
+                
             }
 
             if(($this->cancel_reason == null) && ($this->cancel_reason_id == null)){
