fd74daacbfde3e44b9c56d146c743881d617919d
trankhanh2010 <tranlenguyenkhanh20102001@gmail.com>
2025-07-29 16:25:30 +0700

    upcode + param danhSachTheoKhoaDieuTri api tracking-list

--- app/Services/Model/TrackingListVViewService.php
+++ app/Services/Model/TrackingListVViewService.php
@@ -80,15 +80,12 @@ class TrackingListVViewService
         $data = $this->trackingListVViewRepository->applyIsDeleteFilter($data, 0);
         $data = $this->trackingListVViewRepository->applyTreatmentIdFilter($data, $this->params->treatmentId);
         $count = null;
-        $this->params->orderBy = [
-            'department_name' => 'asc',
-            'tracking_time' => 'desc',
-        ];
         $data = $this->trackingListVViewRepository->applyOrdering($data, $this->params->orderBy, []);
         $data = $this->trackingListVViewRepository->fetchData($data, $this->params->getAll, $this->params->start, $this->params->limit);
         // Group theo field
-        $this->params->groupBy = ['departmentName'];
+        $this->params->groupBy = ['departmentName', 'intructionDate'];
         $data = $this->trackingListVViewRepository->applyGroupByField($data, $this->params->groupBy);
+        // Lấy và thêm những dịch vụ không có tờ điều trị vào danh sách tờ điều trị (id sẽ là null và có thêm phần children chứa những dịch vụ ở trong), được nhóm theo khoa và ngày
         $dataNotInTracking = $this->getSereServNotInTracking($this->params->treatmentId);
 
         $data = $this->addNotInTracking($data, $dataNotInTracking);
@@ -105,11 +102,12 @@ class TrackingListVViewService
             return $data;
         }
         // Đảm bảo $data là mảng
-        $data = is_array($data) ? $data : $data->toArray();
+        $data =  $this->deepToArray($data);
+        $dataNotInTracking = is_array($dataNotInTracking['data']) ? $dataNotInTracking['data'] : $dataNotInTracking['data']->toArray();
+        foreach ($dataNotInTracking as $key => $item) {
+            $deptName = $item['requestDepartmentName'];
+            $intructionDate = $item['intructionDate'];
 
-        foreach ($dataNotInTracking['data'] as $item) {
-            $deptName = $item->request_department_name;
-            $intructionDate = $item->intruction_date;
             // Tìm vị trí của departmentName trong $data
             $deptIndex = collect($data)->search(function ($d) use ($deptName) {
                 return $d['departmentName'] === $deptName;
@@ -121,27 +119,62 @@ class TrackingListVViewService
                     'key' => $deptName,
                     'departmentName' => $deptName,
                     'total' => 0,
-                    'children' => [],
+                    'children' => [
+                        [
+                            'key' => $deptName.$intructionDate,
+                            'intructionDate' => $intructionDate,
+                            'total' => 0,
+                            'children' => [],
+                        ],
+                    ]
                 ];
                 end($data);
-                $deptIndex = key($data);
+                $deptIndex = array_key_last($data);
             }
+            // Tìm vị trí của intructionDate trong data của $data[$deptIndex]['children']
+            $intructionDateIndex = collect($data[$deptIndex]['children'])->search(function ($d) use ($intructionDate) {
+                return $d['intructionDate'] === $intructionDate;
+            });
 
-            // Tham chiếu trực tiếp tới group children
-            $children = &$data[$deptIndex]['children'];
+            // Nếu không tìm thấy, tạo mới
+            if ($intructionDateIndex === false) {
+                $data[$deptIndex]['children'][] = [
+                    'key' => $deptName.$intructionDate,
+                    'intructionDate' => $intructionDate,
+                    'total' => 0,
+                    'children' => [
+                        [
+                            'key' => $deptName.$intructionDate.'_not_in_tracking',
+                            'id' => null,
+                            'creator' => null,
+                            'trackingTime' => null,
+                            'icdCode' => null,
+                            'icdName' => null,
+                            'content' => null,
+                            'departmentName' => $deptName,
+                            'intructionDate' => $intructionDate,
+                            'trackingCreator' => null,
+                            'services' => [], // chứa danh sách dịch vụ
+                        ]
+                    ],
+                ];
+                end($data[$deptIndex]['children']);
+                $intructionDateIndex = array_key_last($data[$deptIndex]['children']);
+            }
+            // Tham chiếu trực tiếp tới group children của intructionDate
+            $children = &$data[$deptIndex]['children'][$intructionDateIndex]['children'];
 
-            // Tìm nhóm instructionDate có id = null
-            $childIndex = collect($children)->search(function ($c) use ($intructionDate) {
-                return empty($c['id']) && $c['intructionDate'] === $intructionDate;
+            // Tìm bản ghi children instructionDate có id = null
+            $childIndex = collect($children)->search(function ($c) use($intructionDate) {
+                return empty($c['id']);
             });
 
             $isFound = $childIndex !== false;
 
             // Nếu chưa có, tạo mới mảng chứa list dịch vụ
             if (!$isFound) {
-
                 $children[] = [
-                    'key' => $deptName.$intructionDate,
+                    'key' => $deptName.$intructionDate.'_not_in_tracking',
                     'id' => null,
                     'creator' => null,
                     'trackingTime' => null,
@@ -162,9 +195,20 @@ class TrackingListVViewService
             if (isset($children[$childIndex]) && is_array($children[$childIndex])) {
                 $children[$childIndex]['services'] = $children[$childIndex]['services'] ?? [];
                 $children[$childIndex]['services'][] = is_array($item) ? $item : $item->toArray();
+            }
+        }
 
-                // Tăng tổng
-                $data[$deptIndex]['total']++;
+        return $data;
+    }
+    public function deepToArray($data)
+    {
+        if ($data instanceof \Illuminate\Support\Collection) {
+            $data = $data->toArray();
+        }
+
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = $this->deepToArray($value);
             }
         }
 
